0f226f9fe21a14e88d0d3eb8693c7fae8c504c4c
NakelCrackk <nakelcrackk@gmail.com>
2026-02-18 00:23:03 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "HIM 💯🙌",
+  botOwner: 'CRACKK💯🤘⚓️', // Your name
+  ownerNumber: '9112001400', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
